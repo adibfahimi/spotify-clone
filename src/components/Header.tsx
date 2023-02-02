2e83cdbcac46e8938c8b9269d36a0a60733d9c71
Adib Fahimi <adibfahiimi@gmail.com>
2023-02-02 21:49:05 +0330

    Fix Header scroll background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,28 +6,8 @@ interface IUser {
 	image: string | null;
 }
 
-function hexWithOpacity(hex: string, opacity: number): string {
-	let r: number;
-	let g: number;
-	let b: number;
-
-	if (opacity > 0.7) opacity = 0.7;
-
-	if (hex.length === 7) {
-		r = parseInt(hex.substring(1, 3), 16);
-		g = parseInt(hex.substring(3, 5), 16);
-		b = parseInt(hex.substring(5, 7), 16);
-	} else {
-		r = parseInt(hex.substring(1, 2), 16);
-		g = parseInt(hex.substring(2, 3), 16);
-		b = parseInt(hex.substring(3, 5), 16);
-	}
-
-	return `rgba(${r}, ${g}, ${b}, ${opacity})`;
-}
-
 export default function Header({ color }: { color: string }) {
-	const [backgrounColor, setBackgrounColor] = useState('transparent');
+	const [opacity, setOpacity] = useState<number>(0);
 	const [user, setUser] = useState<IUser>({
 		name: '',
 		image: null,
@@ -35,49 +15,55 @@ export default function Header({ color }: { color: string }) {
 
 	useEffect(() => {
 		setUser({
-			name: 'John Doe',
+			name: 'Adib Fhaimi',
 			image: null,
 		});
 	}, []);
 
 	if (typeof window !== 'undefined') {
 		window.addEventListener('scroll', () => {
-			if (window.scrollY > 100) {
-				setBackgrounColor(color);
-			}
+			setOpacity(window.scrollY / 100);
 		});
 	}
 
 	return (
 		<header
-			className="op fixed top-0 right-0 z-50 flex h-16 w-full justify-between py-4 px-8 "
+			className="op fixed top-0 right-0 z-50 h-16 w-full"
 			style={{
 				width: 'calc(100% - 218px)',
-				backgroundColor: hexWithOpacity(backgrounColor, window.scrollY / 100),
 			}}
 		>
-			<div className="flex gap-4">
-				<button className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-[#000000b3] opacity-60">
-					<ChevronRightIcon />
-				</button>
-				<button className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-[#000000b3] opacity-60">
-					<ChevronLeftIcon />
-				</button>
-			</div>
+			<div
+				className=" -z-10 absolute top-0 right-0 h-full w-full"
+				style={{
+					backgroundColor: color,
+					opacity: opacity,
+				}}
+			></div>
+			<div className="flex justify-between py-4 px-8">
+				<div className="flex gap-4">
+					<button className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-[#000000b3] opacity-60">
+						<ChevronRightIcon />
+					</button>
+					<button className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-[#000000b3] opacity-60">
+						<ChevronLeftIcon />
+					</button>
+				</div>
 
-			<div className="flex gap-6">
-				<button className="flex min-h-[32px] items-center justify-center rounded-full border border-[#727272] bg-transparent px-[15px] font-bold">
-					Upgrade
-				</button>
-				<button className="flex h-8 items-center gap-2 rounded-full bg-[#000000b3] p-[2px]">
-					<div className="flex h-7 w-7 items-center justify-center rounded-full bg-[#535353]">
-						<ProfileIcon />
-					</div>
-					<span className="font-bold">{user ? user.name : 'Loading...'}</span>
-					<div className="mr-1">
-						<ChevronDownIcon />
-					</div>
-				</button>
+				<div className="flex gap-6">
+					<button className="flex min-h-[32px] items-center justify-center rounded-full border border-[#727272] bg-transparent px-[15px] font-bold">
+						Upgrade
+					</button>
+					<button className="flex h-8 items-center gap-2 rounded-full bg-[#000000b3] p-[2px]">
+						<div className="flex h-7 w-7 items-center justify-center rounded-full bg-[#535353]">
+							<ProfileIcon />
+						</div>
+						<span className="font-bold">{user ? user.name : 'Loading...'}</span>
+						<div className="mr-1">
+							<ChevronDownIcon />
+						</div>
+					</button>
+				</div>
 			</div>
 		</header>
 	);
